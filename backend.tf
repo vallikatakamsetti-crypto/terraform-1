721aa51d896c091f628883451f947248872e8406
valli <valli.katakamsetti@gmail.com>
2026-06-10 22:52:16 +0530

    first push

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "storesample-tfstatefile"
+    key = "s3_statefile"
+    use_lockfile = true
+    region = "us-east-1"
+    
+  }
+}
---
+++ s3_main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "store_app" {
+    bucket = "application_bucket"
+    
+}
